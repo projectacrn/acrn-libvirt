eb2968a9d3411179f67fb965fa92b17b0d77320d
Xiangyang Wu <xiangyang.wu@intel.com>
2022-02-27 12:56:37 +0800

    acrn-driver: support shutdown user VM gracefully
    The following updates are maded to support shutdown user
    VM gracefully:
    - Implement a acrn manager to interact with ACRN lifecycle
    manager.
    - Send user_vm_shutdown command to ACRN lifecycle manager to
    shutdown a specified user VM.
    
    Tracked-On: #5921
    
    Signed-off-by: Xiangyang Wu <xiangyang.wu@intel.com>

---
+++ src/acrn/acrn_manager.h
@@ -0,0 +1,38 @@
+#ifndef __ACRN_MANAGER_H__
+#define __ACRN_MANAGER_H__
+
+#include "domain_conf.h"
+
+typedef struct _acrnManager acrnManager;
+typedef acrnManager *acrnManagerPtr;
+
+typedef void (*acrnManagerStopCallback)(virDomainObjPtr vm);
+typedef struct _acrnManagerMessage acrnManagerMessage;
+typedef acrnManagerMessage *acrnManagerMessagePtr;
+
+struct _acrnManagerMessage {
+    int txFD;
+
+    char *txBuffer;
+    int txOffset;
+    int txLength;
+
+    /* Used by the text monitor reply / error */
+    char *rxBuffer;
+    int rxLength;
+    /* Used by the JSON monitor to hold reply / error */
+    void *rxObject;
+
+    /* True if rxBuffer / rxObject are ready, or a
+     * fatal error occurred on the monitor channel
+     */
+    bool finished;
+};
+
+int acrnManagerSystemPowerdown(acrnManagerPtr mon);
+acrnManagerPtr acrnManagerOpen(virDomainObjPtr vm, virDomainChrSourceDefPtr config, acrnManagerStopCallback cb);
+void acrnManagerClose(acrnManagerPtr mon);
+bool acrnManagerRegister(acrnManagerPtr mon);
+void acrnManagerUnregister(acrnManagerPtr mon);
+int acrnManagerGetReason(acrnManagerPtr mon);
+#endif /* __ACRN_MANAGER_H__ */
